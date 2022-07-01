bada44e20889ef200ecf853e0026a38ab1259785
thewakar <thewakar1@gmail.com>
2022-07-01 16:24:09 +0530

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-row items-center justify-center mt-4 mb-4">
   <div>
-    Built by <a href="https://twitter.com/alibuilds" class="underline underline-offset-1"
-      >Ali Hussain</a
+    Built by <a href="https://twitter.com/newwakar" class="underline underline-offset-1"
+      >TheWakar</a
     >
   </div>
 </div>
